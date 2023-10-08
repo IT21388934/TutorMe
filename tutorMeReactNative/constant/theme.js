91a3b8d73ca8b277b2b9ed08a148e1cf08bc7cbc
IT21388934 <it21388934@my.sliit.lk>
2023-10-08 11:40:54 +0530

    set constant, upload images, upload fonts

---
+++ tutorMeReactNative/constant/theme.js
@@ -0,0 +1,35 @@
+const COLORS = {
+  green: "#14A056", //login background, button background
+  backgroundGreen: "#C3FCDE", //background
+  lightGreen: "#C1FDDD", //tags
+  darkGreen: "#052D18", //login background
+
+  white: "#FFFFFF", // fonts
+  black: "#1B262E", // fonts
+  lightGray: "#E7ECF0", // fonts
+  darkGray: "#414141", // fonts
+
+  orange: "#FA5804", // buttons, contrast color
+
+  blueButton: "#414141", // buttons
+  redButton: "#FB3D3D", // buttons
+
+  warningYellow: "#F8CD20", //warning notification
+};
+
+const FONT = {
+  regular: "DMRegular",
+  medium: "DMMedium",
+  bold: "DMBold",
+};
+
+const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 32,
+};
+
+export { COLORS, FONT, SIZES };
