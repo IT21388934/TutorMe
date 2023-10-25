5e56bc517ddfdb0272994c6dc1d01da9452bc248
S. A. D. S. Kumarathunga <it21118340@my.sliit.lk>
2023-10-25 23:54:51 +0530

    Update Navigation.js

--- tutorMeReactNative/src/navigation/Navigation.js
+++ tutorMeReactNative/src/navigation/Navigation.js
@@ -1,10 +1,9 @@
-import React, { useContext } from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { useEffect, useState } from "react";
-import { FIREBASE_AUTH } from "../../FirebaseConfig";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { onAuthStateChanged } from "firebase/auth";
+import React, { useContext, useEffect, useState } from "react";
+import { FIREBASE_AUTH } from "../../FirebaseConfig";
 import UserContext from "../contexts/UserContext";
 
 //common
@@ -14,142 +13,146 @@ import StudentSignUp from "../screens/StudentSignUp";
 import TutorSignUp from "../screens/TutorSignUp";
 
 //student
-import StudentHome from "../screens/StudentScreens/StudentHome";
 import CategorySearch from "../screens/StudentScreens/CategorySearch";
-import StudyMaterials from "../screens/StudentScreens/StudyMaterials";
-import StudentProfile from "../screens/StudentScreens/StudentProfile";
 import ClassSearchResults from "../screens/StudentScreens/ClassSearchResults";
-import RequestSession from "../screens/StudentScreens/RequestSession";
-import MySessions from "../screens/StudentScreens/MySessions";
-import SessionDetails from "../screens/StudentScreens/SessionDetails";
 import EditSession from "../screens/StudentScreens/EditSession";
+import MySessions from "../screens/StudentScreens/MySessions";
+import RequestSession from "../screens/StudentScreens/RequestSession";
+import SessionDetails from "../screens/StudentScreens/SessionDetails";
+import StudentHome from "../screens/StudentScreens/StudentHome";
+import SMDetails from "../screens/StudentScreens/StudentMaterialsDetails";
+import StudentProfile from "../screens/StudentScreens/StudentProfile";
+import StudyMaterials from "../screens/StudentScreens/StudyMaterials";
 
 //tutor
-import TutorHome from "../screens/TutorScreens/TutorHome";
-import MyClasses from "../screens/TutorScreens/MyClasses";
-import Calendar from "../screens/TutorScreens/Calendar";
-import TutorProfile from "../screens/TutorScreens/TutorProfile";
-import NewRequests from "../screens/TutorScreens/NewRequests";
-import SingleNewRequest from "../screens/TutorScreens/SingleNewRequest";
-import ClassDetails from "../screens/TutorScreens/ClassDetails";
 import AddClass from "../screens/TutorScreens/AddClass";
-import EditClassDetails from "../screens/TutorScreens/EditClassDetails";
-import TutorSessions from "../screens/TutorScreens/TutorSessions";
 import BookedClasses from "../screens/TutorScreens/BookedClasses";
-import SingleBooked from "../screens/TutorScreens/SingleBooked";
+import Calendar from "../screens/TutorScreens/Calendar";
+import ClassDetails from "../screens/TutorScreens/ClassDetails";
 import CompletedClasses from "../screens/TutorScreens/CompletedClasses";
+import EditClassDetails from "../screens/TutorScreens/EditClassDetails";
+import MyClasses from "../screens/TutorScreens/MyClasses";
+import NewRequests from "../screens/TutorScreens/NewRequests";
+import SingleBooked from "../screens/TutorScreens/SingleBooked";
+import SingleNewRequest from "../screens/TutorScreens/SingleNewRequest";
+import TutorHome from "../screens/TutorScreens/TutorHome";
+import TutorProfile from "../screens/TutorScreens/TutorProfile";
+import TutorSessions from "../screens/TutorScreens/TutorSessions";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function TutorTabs() {
-  return (
-    <Tab.Navigator screenOptions={{ headerShown: false }} tabBar={() => null}>
-      <Tab.Screen name="home" component={TutorHome} />
-      <Tab.Screen name="myClasses" component={MyClasses} />
-      <Tab.Screen name="calendar" component={Calendar} />
-      <Tab.Screen name="profile" component={TutorProfile} />
-      <Tab.Screen name="newRequests" component={NewRequests} />
-      <Tab.Screen name="singleNewRequest" component={SingleNewRequest} />
-      <Tab.Screen name="classDetails" component={ClassDetails} />
-      <Tab.Screen name="addClass" component={AddClass} />
-      <Tab.Screen name="editClassDetails" component={EditClassDetails} />
-      <Tab.Screen name="mySessions" component={TutorSessions} />
-      <Tab.Screen name="bookedClasses" component={BookedClasses} />
-      <Tab.Screen name="singleBooked" component={SingleBooked} />
-      <Tab.Screen name="completedClasses" component={CompletedClasses} />
-    </Tab.Navigator>
-  );
+	return (
+		<Tab.Navigator screenOptions={{ headerShown: false }} tabBar={() => null}>
+			<Tab.Screen name="home" component={TutorHome} />
+			<Tab.Screen name="myClasses" component={MyClasses} />
+			<Tab.Screen name="calendar" component={Calendar} />
+			<Tab.Screen name="profile" component={TutorProfile} />
+			<Tab.Screen name="newRequests" component={NewRequests} />
+			<Tab.Screen name="singleNewRequest" component={SingleNewRequest} />
+			<Tab.Screen name="classDetails" component={ClassDetails} />
+			<Tab.Screen name="addClass" component={AddClass} />
+			<Tab.Screen name="editClassDetails" component={EditClassDetails} />
+			<Tab.Screen name="mySessions" component={TutorSessions} />
+			<Tab.Screen name="bookedClasses" component={BookedClasses} />
+			<Tab.Screen name="singleBooked" component={SingleBooked} />
+			<Tab.Screen name="completedClasses" component={CompletedClasses} />
+		</Tab.Navigator>
+	);
 }
 
 function TutorInside() {
-  return (
-    <Stack.Navigator
-      screenOptions={{ headerShown: false }}
-      initialRouteName="main"
-    >
-      <Stack.Screen name="main" component={TutorTabs} />
-      {/* other tutor screeens which doesn't contain bottom navigation bar */}
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator
+			screenOptions={{ headerShown: false }}
+			initialRouteName="main"
+		>
+			<Stack.Screen name="main" component={TutorTabs} />
+			{/* other tutor screeens which doesn't contain bottom navigation bar */}
+		</Stack.Navigator>
+	);
 }
 
 function StudentTabs() {
-  return (
-    <Tab.Navigator screenOptions={{ headerShown: false }} tabBar={() => null}>
-      <Tab.Screen name="home" component={StudentHome} />
-      <Tab.Screen name="search" component={CategorySearch} />
-      <Tab.Screen name="studyMaterials" component={StudyMaterials} />
-      <Tab.Screen name="profile" component={StudentProfile} />
-    </Tab.Navigator>
-  );
+	return (
+		<Tab.Navigator screenOptions={{ headerShown: false }} tabBar={() => null}>
+			<Tab.Screen name="home" component={StudentHome} />
+			<Tab.Screen name="search" component={CategorySearch} />
+			<Tab.Screen name="studyMaterials" component={StudyMaterials} />
+			<Tab.Screen name="profile" component={StudentProfile} />
+		</Tab.Navigator>
+	);
 }
 
 function StudentInside() {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-      initialRouteName="main"
-    >
-      <Stack.Screen name="main" component={StudentTabs} />
-      {/* other student screeens which doesn't contain bottom navigation bar */}
-      <Stack.Screen name="requestSession" component={RequestSession} />
-      <Stack.Screen name="mySessions" component={MySessions} />
-      <Stack.Screen name="sessionDetails" component={SessionDetails} />
-      <Stack.Screen name="searchResults" component={ClassSearchResults} />
-      <Stack.Screen name="editSession" component={EditSession} />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+			initialRouteName="main"
+		>
+			<Stack.Screen name="main" component={StudentTabs} />
+			{/* other student screeens which doesn't contain bottom navigation bar */}
+			<Stack.Screen name="requestSession" component={RequestSession} />
+			<Stack.Screen name="mySessions" component={MySessions} />
+			<Stack.Screen name="sessionDetails" component={SessionDetails} />
+			<Stack.Screen name="searchResults" component={ClassSearchResults} />
+			<Stack.Screen name="editSession" component={EditSession} />
+		</Stack.Navigator>
+	);
 }
 
 function CommonTabs() {
-  return (
-    <Tab.Navigator screenOptions={{ headerShown: false }} tabBar={() => null}>
-      <Tab.Screen name="login" component={Login} />
-      <Tab.Screen name="signUpOptions" component={SignUpOptions} />
-      <Tab.Screen name="studentSignUp" component={StudentSignUp} />
-      <Tab.Screen name="tutorSignUp" component={TutorSignUp} />
-    </Tab.Navigator>
-  );
+	return (
+		<Tab.Navigator screenOptions={{ headerShown: false }} tabBar={() => null}>
+			<Tab.Screen name="login" component={Login} />
+			<Tab.Screen name="signUpOptions" component={SignUpOptions} />
+			<Tab.Screen name="studentSignUp" component={StudentSignUp} />
+			<Tab.Screen name="tutorSignUp" component={TutorSignUp} />
+		</Tab.Navigator>
+	);
 }
 
 function CommonInside() {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-      initialRouteName="login"
-    >
-      <Stack.Screen name="main" component={CommonTabs} />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+			initialRouteName="login"
+		>
+			<Stack.Screen name="main" component={CommonTabs} />
+		</Stack.Navigator>
+	);
 }
 
 export default function Navigation() {
-  // const [user, setUser] = useState(null); //set useState() for login screen
-  // const [userType, setUserType] = useState("tutor"); //switch between student and tutor
+	// const [user, setUser] = useState(null); //set useState() for login screen
+	// const [userType, setUserType] = useState("tutor"); //switch between student and tutor
 
-  const { userData, setUserData } = useContext(UserContext);
+	const { userData, setUserData } = useContext(UserContext);
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="login"
-        screenOptions={{ headerShown: false, statusBarColor: "green" }}
-      >
-        {userData !== null ? (
-          userData.role === "student" ? (
-            <Stack.Screen name="studentInside" component={StudentInside} />
-          ) : (
-            <Stack.Screen name="tutorInside" component={TutorInside} />
-          )
-        ) : (
-          <Stack.Screen name="commonInside" component={CommonInside} />
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				initialRouteName="login"
+				screenOptions={{ headerShown: false, statusBarColor: "green" }}
+			>
+				{userData !== null ? (
+					userData.role === "student" ? (
+						<Stack.Screen
+							name="studentInside"
+							component={StudentInside}
+						/>
+					) : (
+						<Stack.Screen name="tutorInside" component={TutorInside} />
+					)
+				) : (
+					<Stack.Screen name="commonInside" component={CommonInside} />
+				)}
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
