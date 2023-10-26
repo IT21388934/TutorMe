1c9624d093fa0e2e4d500ad431e01b124eeab613
S. A. D. S. Kumarathunga <it21118340@my.sliit.lk>
2023-10-26 13:57:39 +0530

    Alerts

--- tutorMeReactNative/src/screens/TutorScreens/StudyMaterials/SMAdd.js
+++ tutorMeReactNative/src/screens/TutorScreens/StudyMaterials/SMAdd.js
@@ -15,13 +15,6 @@ import {
 	TouchableOpacity,
 	View,
 } from "react-native";
-import {
-	ALERT_TYPE,
-	AlertNotificationRoot,
-	Dialog,
-	Toast,
-} from "react-native-alert-notification";
-
 import { FIRESTORE_DB } from "../../../../FirebaseConfig";
 import { COLORS } from "../../../constants/theme";
 import { TutorFragment } from "../../../layouts/TutorFragment";
@@ -67,11 +60,17 @@ const SMAdd = () => {
 			selectedImage == null ||
 			selectedPDF == null
 		) {
-			Toast.show({
-				type: ALERT_TYPE.WARNING,
-				title: "Warning",
-				textBody: "!!..Please complete all required fields.",
-			});
+			Alert.alert(
+				"Form Validation",
+				"!!..Please complete all required fields.",
+				[
+					{
+						text: "OK",
+						style: "cancel",
+					},
+				],
+				{ cancelable: false }
+			);
 		} else {
 			setIsLoading(true);
 			const doc = await addDoc(
@@ -79,11 +78,17 @@ const SMAdd = () => {
 				values
 			);
 			clearAll();
-			Toast.show({
-				type: ALERT_TYPE.SUCCESS,
-				title: "Success",
-				textBody: "Operation Successful.!!",
-			});
+			Alert.alert(
+				"Add Item",
+				"Operation Successful.!!",
+				[
+					{
+						text: "OK",
+						style: "cancel",
+					},
+				],
+				{ cancelable: false }
+			);
 			setIsLoading(false);
 			navigation.navigate("smList");
 		}
@@ -122,195 +127,181 @@ const SMAdd = () => {
 	};
 
 	return (
-		<AlertNotificationRoot>
-			<>
-				<TutorFragment activeLink="profile">
-					{/*  start header */}
-					<View style={{ backgroundColor: COLORS.green, height: "10%" }}>
-						<Text
-							style={{
-								color: "white",
-								paddingVertical: 19,
-								paddingHorizontal: 125,
-								fontSize: 25,
-							}}
-						>
-							Add Items
-						</Text>
-					</View>
-					{/*  end header */}
+		<TutorFragment activeLink="profile">
+			{/*  start header */}
+			<View style={{ backgroundColor: COLORS.green, height: "10%" }}>
+				<Text
+					style={{
+						color: "white",
+						paddingVertical: 19,
+						paddingHorizontal: 125,
+						fontSize: 25,
+					}}
+				>
+					Add Items
+				</Text>
+			</View>
+			{/*  end header */}
 
-					{/*  start content */}
-					<ScrollView
+			{/*  start content */}
+			<ScrollView
+				style={{
+					marginTop: "40px",
+					padding: 10,
+				}}
+			>
+				{/* Text Input for Book Name */}
+				<Text style={styles.inputLabel}>Title: </Text>
+				<TextInput
+					id="smTitle"
+					class="smTitle"
+					style={styles.input}
+					placeholder="📖  |  Title"
+					placeholderTextColor={COLORS.darkGray}
+					onChangeText={(text) => setValues({ ...values, title: text })}
+					value={values.title}
+				/>
+
+				{/* Text Input for Description */}
+				<Text style={styles.inputLabel}>Description: </Text>
+				<TextInput
+					id="smDesc"
+					class="smDesc"
+					style={styles.input}
+					placeholder="📖  |  Description"
+					placeholderTextColor={COLORS.darkGray}
+					multiline={true}
+					numberOfLines={5}
+					onChangeText={(text) => setValues({ ...values, desc: text })}
+					value={values.desc}
+				/>
+
+				{/* Picker Input for Category */}
+				<Text style={styles.inputLabel}>Category: </Text>
+				<Picker
+					selectedValue={selectedValue}
+					style={styles.input}
+					onValueChange={(itemValue) => categoryFunc(itemValue)}
+				>
+					<Picker.Item label="🗂 | Select Category" value="Category" />
+					<Picker.Item label="Mathematics" value="Mathematics" />
+					<Picker.Item label="English" value="English" />
+					<Picker.Item label="Computer Science" value="Computer Science" />
+					<Picker.Item label="Biology" value="Biology" />
+					<Picker.Item label="Engineering" value="Engineering" />
+					<Picker.Item label="Other" value="Other" />
+				</Picker>
+
+				{/* Upload Image */}
+				<TouchableOpacity
+					style={styles.buttonImage}
+					onPress={() => pickImage()}
+				>
+					{selectedImage ? (
+						<Text style={styles.buttonText}> ✔ Upload Image</Text>
+					) : (
+						<Text style={styles.buttonText}>Upload Image</Text>
+					)}
+				</TouchableOpacity>
+				{selectedImage ? (
+					<View
 						style={{
-							marginTop: "40px",
-							padding: 10,
+							flexDirection: "column",
+							alignItems: "center",
+							marginTop: 5,
 						}}
 					>
-						{/* Text Input for Book Name */}
-						<Text style={styles.inputLabel}>Title: </Text>
-						<TextInput
-							id="smTitle"
-							class="smTitle"
-							style={styles.input}
-							placeholder="📖  |  Title"
-							placeholderTextColor={COLORS.darkGray}
-							onChangeText={(text) =>
-								setValues({ ...values, title: text })
-							}
-							value={values.title}
+						<Image
+							resizeMode="contain"
+							style={{
+								width: 100,
+								height: 100,
+								borderRadius: 10,
+								marginTop: 5,
+							}}
+							source={{
+								uri: selectedImage,
+							}}
 						/>
+					</View>
+				) : (
+					<View
+						style={{
+							flexDirection: "column",
+							alignItems: "center",
+							marginTop: 5,
+						}}
+					>
+						<Text>Add Image</Text>
+					</View>
+				)}
 
-						{/* Text Input for Description */}
-						<Text style={styles.inputLabel}>Description: </Text>
-						<TextInput
-							id="smDesc"
-							class="smDesc"
-							style={styles.input}
-							placeholder="📖  |  Description"
-							placeholderTextColor={COLORS.darkGray}
-							multiline={true}
-							numberOfLines={5}
-							onChangeText={(text) =>
-								setValues({ ...values, desc: text })
-							}
-							value={values.desc}
-						/>
+				{/* upload document */}
+				<TouchableOpacity
+					style={styles.buttonImage}
+					onPress={() => pickDocument()}
+				>
+					{selectedPDF ? (
+						<Text style={styles.buttonText}> ✔ Upload PDF</Text>
+					) : (
+						<Text style={styles.buttonText}>Upload PDF</Text>
+					)}
+				</TouchableOpacity>
+				{selectedPDF ? (
+					<View
+						style={{
+							flexDirection: "column",
+							alignItems: "center",
+							marginTop: 5,
+						}}
+					>
+						<Text>PDF File: {selectedPDF}</Text>
+					</View>
+				) : (
+					<View
+						style={{
+							flexDirection: "column",
+							alignItems: "center",
+							marginTop: 5,
+						}}
+					>
+						<Text>Add PDF File</Text>
+					</View>
+				)}
 
-						{/* Picker Input for Category */}
-						<Text style={styles.inputLabel}>Category: </Text>
-						<Picker
-							selectedValue={selectedValue}
-							style={styles.input}
-							onValueChange={(itemValue) => categoryFunc(itemValue)}
-						>
-							<Picker.Item
-								label="🗂 | Select Category"
-								value="Category"
-							/>
-							<Picker.Item label="Mathematics" value="Mathematics" />
-							<Picker.Item label="English" value="English" />
-							<Picker.Item
-								label="Computer Science"
-								value="Computer Science"
-							/>
-							<Picker.Item label="Biology" value="Biology" />
-							<Picker.Item label="Engineering" value="Engineering" />
-							<Picker.Item label="Other" value="Other" />
-						</Picker>
+				{/* Add button */}
+				<TouchableOpacity
+					style={styles.buttonSubmit}
+					onPress={() => addFunc()}
+				>
+					{isLoading ? (
+						<ActivityIndicator size="large" color="white" />
+					) : (
+						<Text style={styles.buttonText}>Submit</Text>
+					)}
+				</TouchableOpacity>
 
-						{/* Upload Image */}
-						<TouchableOpacity
-							style={styles.buttonImage}
-							onPress={() => pickImage()}
-						>
-							{selectedImage ? (
-								<Text style={styles.buttonText}> ✔ Upload Image</Text>
-							) : (
-								<Text style={styles.buttonText}>Upload Image</Text>
-							)}
-						</TouchableOpacity>
-						{selectedImage ? (
-							<View
-								style={{
-									flexDirection: "column",
-									alignItems: "center",
-									marginTop: 5,
-								}}
-							>
-								<Image
-									resizeMode="contain"
-									style={{
-										width: 100,
-										height: 100,
-										borderRadius: 10,
-										marginTop: 5,
-									}}
-									source={{
-										uri: selectedImage,
-									}}
-								/>
-							</View>
-						) : (
-							<View
-								style={{
-									flexDirection: "column",
-									alignItems: "center",
-									marginTop: 5,
-								}}
-							>
-								<Text>Add Image</Text>
-							</View>
-						)}
+				{/* Reset button */}
+				<TouchableOpacity
+					style={styles.buttonReset}
+					onPress={() => clearAll()}
+				>
+					<Text style={styles.buttonText}>Reset</Text>
+				</TouchableOpacity>
 
-						{/* upload document */}
-						<TouchableOpacity
-							style={styles.buttonImage}
-							onPress={() => pickDocument()}
-						>
-							{selectedPDF ? (
-								<Text style={styles.buttonText}> ✔ Upload PDF</Text>
-							) : (
-								<Text style={styles.buttonText}>Upload PDF</Text>
-							)}
-						</TouchableOpacity>
-						{selectedPDF ? (
-							<View
-								style={{
-									flexDirection: "column",
-									alignItems: "center",
-									marginTop: 5,
-								}}
-							>
-								<Text>PDF File: {selectedPDF}</Text>
-							</View>
-						) : (
-							<View
-								style={{
-									flexDirection: "column",
-									alignItems: "center",
-									marginTop: 5,
-								}}
-							>
-								<Text>Add PDF File</Text>
-							</View>
-						)}
+				{/* Cancel button */}
+				<TouchableOpacity
+					style={styles.buttonCancel}
+					onPress={() => navigation.navigate("smList")}
+				>
+					<Text style={styles.buttonText}>Cancel</Text>
+				</TouchableOpacity>
 
-						{/* Add button */}
-						<TouchableOpacity
-							style={styles.buttonSubmit}
-							onPress={() => addFunc()}
-						>
-							{isLoading ? (
-								<ActivityIndicator size="large" color="white" />
-							) : (
-								<Text style={styles.buttonText}>Submit</Text>
-							)}
-						</TouchableOpacity>
-
-						{/* Reset button */}
-						<TouchableOpacity
-							style={styles.buttonReset}
-							onPress={() => clearAll()}
-						>
-							<Text style={styles.buttonText}>Reset</Text>
-						</TouchableOpacity>
-
-						{/* Cancel button */}
-						<TouchableOpacity
-							style={styles.buttonCancel}
-							onPress={() => navigation.navigate("smList")}
-						>
-							<Text style={styles.buttonText}>Cancel</Text>
-						</TouchableOpacity>
-
-						<TouchableOpacity style={styles.buttonInvisble}>
-							<Text style={styles.buttonText}></Text>
-						</TouchableOpacity>
-					</ScrollView>
-				</TutorFragment>
-			</>
-		</AlertNotificationRoot>
+				<TouchableOpacity style={styles.buttonInvisble}>
+					<Text style={styles.buttonText}></Text>
+				</TouchableOpacity>
+			</ScrollView>
+		</TutorFragment>
 	);
 };
 
