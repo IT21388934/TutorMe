fa758adff0c55521358b6c5b56132941338629d7
S. A. D. S. Kumarathunga <it21118340@my.sliit.lk>
2023-10-26 14:01:33 +0530

    Update SMViewDetails.js

--- tutorMeReactNative/src/screens/TutorScreens/StudyMaterials/SMViewDetails.js
+++ tutorMeReactNative/src/screens/TutorScreens/StudyMaterials/SMViewDetails.js
@@ -13,6 +13,12 @@ import {
 	TouchableOpacity,
 	View,
 } from "react-native";
+import {
+	ALERT_TYPE,
+	AlertNotificationRoot,
+	Dialog,
+	Toast,
+} from "react-native-alert-notification";
 import { FIRESTORE_DB } from "../../../../FirebaseConfig";
 import images from "../../../constants/images";
 import { COLORS } from "../../../constants/theme";
@@ -38,20 +44,6 @@ const SMView = () => {
 	const fileUri = `${FileSystem.documentDirectory}${filename}`;
 	const uri = values.pdfLink;
 
-	const AlertCustom = (title, msg) => {
-		Alert.alert(
-			title,
-			msg,
-			[
-				{
-					text: "Ok",
-					style: "ok",
-				},
-			],
-			{ cancelable: false }
-		);
-	};
-
 	const downDoc = async () => {
 		setDownloading(true);
 
@@ -61,7 +53,11 @@ const SMView = () => {
 		).downloadAsync();
 
 		if ((await downloadedFile).status != 200) {
-			alert("Download Error");
+			Toast.show({
+				type: ALERT_TYPE.DANGER,
+				title: "Error",
+				textBody: "Download failed..!!",
+			});
 		}
 
 		const permissions =
@@ -91,10 +87,11 @@ const SMView = () => {
 			await updateDoc(docRef, { downloads: values.downloads + 1 });
 			setValues({ ...values, downloads: values.downloads + 1 });
 			setDownloading(false);
-			AlertCustom(
-				"Download PDF",
-				`Download Completed. PDF is saved to your device.`
-			);
+			Toast.show({
+				type: ALERT_TYPE.SUCCESS,
+				title: "Download Complete",
+				textBody: "File downloaded successfully..!!",
+			});
 		} catch (e) {
 			console.log(e);
 		}
@@ -118,162 +115,166 @@ const SMView = () => {
 
 	// render screen
 	return (
-		<TutorFragment activeLink="profile">
-			<View
-				style={{
-					backgroundColor: COLORS.green,
-					height: "10%",
-					display: "flex",
-					alignItems: "center",
-					flexDirection: "row",
-				}}
-			>
-				<TouchableOpacity
-					onPress={() => navigation.navigate("studyMaterials")}
-				>
-					<Image
-						source={images.backButton}
-						style={{ width: 25, height: 25, marginLeft: 5 }}
-					></Image>
-				</TouchableOpacity>
-				<Text
-					style={{
-						color: "white",
-						fontSize: 25,
-						textAlign: "center",
-						paddingHorizontal: 95,
-					}}
-				>
-					View Details
-				</Text>
-			</View>
-			<View
-				style={{
-					flexDirection: "column",
-					alignItems: "center",
-					marginTop: 20,
-				}}
-			>
-				<Text style={{ fontWeight: "bold", fontSize: 25 }}>
-					{values.title}
-				</Text>
-				<Image
-					resizeMode="contain"
-					style={{
-						width: 200,
-						height: 200,
-						borderRadius: 10,
-						marginTop: 15,
-						marginBottom: 25,
-						shadowColor: "#000",
-						shadowOffset: { width: 0, height: 2 },
-						shadowOpacity: 0.2,
-						shadowRadius: 3.84,
-						elevation: 4,
-					}}
-					source={{
-						uri: values.imgLink,
-					}}
-				/>
-			</View>
-			<View
-				style={{
-					flexDirection: "column",
-					alignItems: "flex-start",
-					marginHorizontal: 5,
-					marginBottom: 10,
-					marginLeft: 25,
-					marginRight: 25,
-					padding: 10,
-					backgroundColor: COLORS.white,
-					borderRadius: 10,
-					shadowColor: "#000",
-					shadowOffset: {
-						width: 0,
-						height: 2,
-					},
-					shadowOpacity: 0.2,
-					shadowRadius: 3.84,
-					elevation: 4,
-				}}
-			>
-				<View style={{ flexDirection: "row", marginVertical: 5 }}>
-					<Text
+		<AlertNotificationRoot>
+			<>
+				<TutorFragment activeLink="profile">
+					<View
 						style={{
-							fontWeight: "bold",
-							fontSize: 18,
+							backgroundColor: COLORS.green,
+							height: "10%",
+							display: "flex",
+							alignItems: "center",
+							flexDirection: "row",
 						}}
 					>
-						Category:
-					</Text>
-					<Text
+						<TouchableOpacity
+							onPress={() => navigation.navigate("studyMaterials")}
+						>
+							<Image
+								source={images.backButton}
+								style={{ width: 25, height: 25, marginLeft: 5 }}
+							></Image>
+						</TouchableOpacity>
+						<Text
+							style={{
+								color: "white",
+								fontSize: 25,
+								textAlign: "center",
+								paddingHorizontal: 95,
+							}}
+						>
+							View Details
+						</Text>
+					</View>
+					<View
 						style={{
-							fontSize: 18,
-							paddingLeft: 25,
+							flexDirection: "column",
+							alignItems: "center",
+							marginTop: 20,
 						}}
 					>
-						{values.category}
-					</Text>
-				</View>
+						<Text style={{ fontWeight: "bold", fontSize: 25 }}>
+							{values.title}
+						</Text>
+						<Image
+							resizeMode="contain"
+							style={{
+								width: 200,
+								height: 200,
+								borderRadius: 10,
+								marginTop: 15,
+								marginBottom: 25,
+								shadowColor: "#000",
+								shadowOffset: { width: 0, height: 2 },
+								shadowOpacity: 0.2,
+								shadowRadius: 3.84,
+								elevation: 4,
+							}}
+							source={{
+								uri: values.imgLink,
+							}}
+						/>
+					</View>
+					<View
+						style={{
+							flexDirection: "column",
+							alignItems: "flex-start",
+							marginHorizontal: 5,
+							marginBottom: 10,
+							marginLeft: 25,
+							marginRight: 25,
+							padding: 10,
+							backgroundColor: COLORS.white,
+							borderRadius: 10,
+							shadowColor: "#000",
+							shadowOffset: {
+								width: 0,
+								height: 2,
+							},
+							shadowOpacity: 0.2,
+							shadowRadius: 3.84,
+							elevation: 4,
+						}}
+					>
+						<View style={{ flexDirection: "row", marginVertical: 5 }}>
+							<Text
+								style={{
+									fontWeight: "bold",
+									fontSize: 18,
+								}}
+							>
+								Category:
+							</Text>
+							<Text
+								style={{
+									fontSize: 18,
+									paddingLeft: 25,
+								}}
+							>
+								{values.category}
+							</Text>
+						</View>
 
-				<View style={{ flexDirection: "row", marginVertical: 5 }}>
-					<Text
-						style={{
-							fontWeight: "bold",
-							fontSize: 18,
-						}}
-					>
-						Downloads:
-					</Text>
-					<Text
-						style={{
-							fontSize: 18,
-							paddingLeft: 10,
-						}}
-					>
-						{values.downloads}
-					</Text>
-				</View>
+						<View style={{ flexDirection: "row", marginVertical: 5 }}>
+							<Text
+								style={{
+									fontWeight: "bold",
+									fontSize: 18,
+								}}
+							>
+								Downloads:
+							</Text>
+							<Text
+								style={{
+									fontSize: 18,
+									paddingLeft: 10,
+								}}
+							>
+								{values.downloads}
+							</Text>
+						</View>
 
-				<View
-					style={{
-						flexDirection: "column",
-						marginVertical: 5,
-						flexWrap: "wrap",
-					}}
-				>
-					<Text
-						style={{
-							fontSize: 18,
-							paddingLeft: 0,
-							flexWrap: "wrap",
-							marginTop: 18,
-						}}
-					>
-						{values.desc}
-					</Text>
-				</View>
-			</View>
+						<View
+							style={{
+								flexDirection: "column",
+								marginVertical: 5,
+								flexWrap: "wrap",
+							}}
+						>
+							<Text
+								style={{
+									fontSize: 18,
+									paddingLeft: 0,
+									flexWrap: "wrap",
+									marginTop: 18,
+								}}
+							>
+								{values.desc}
+							</Text>
+						</View>
+					</View>
 
-			<TouchableOpacity
-				style={styles.buttonSubmit}
-				onPress={() => downDoc()}
-			>
-				{isDownloading ? (
-					<ActivityIndicator size="large" color="white" />
-				) : (
-					<Text style={styles.buttonTextDownload}>Download</Text>
-				)}
-			</TouchableOpacity>
-			<TouchableOpacity
-				style={styles.buttonCancel}
-				onPress={() =>
-					navigation.navigate("smEdit", { id: route.params.id })
-				}
-			>
-				<Text style={styles.buttonTextEdit}>Edit</Text>
-			</TouchableOpacity>
-		</TutorFragment>
+					<TouchableOpacity
+						style={styles.buttonSubmit}
+						onPress={() => downDoc()}
+					>
+						{isDownloading ? (
+							<ActivityIndicator size="large" color="white" />
+						) : (
+							<Text style={styles.buttonTextDownload}>Download</Text>
+						)}
+					</TouchableOpacity>
+					<TouchableOpacity
+						style={styles.buttonCancel}
+						onPress={() =>
+							navigation.navigate("smEdit", { id: route.params.id })
+						}
+					>
+						<Text style={styles.buttonTextEdit}>Edit</Text>
+					</TouchableOpacity>
+				</TutorFragment>
+			</>
+		</AlertNotificationRoot>
 	);
 };
 
